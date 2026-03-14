89d8f3eb3798a3cc3f07d9ce9946d98ebe6fbea1
Luz Aramburo <luzaramburo13@gmail.com>
2026-03-14 10:18:09 -0700

    feat: add application icon support for the desktop app and configure BrowserWindow to use favicon

--- packages/desktop/index.js
+++ packages/desktop/index.js
@@ -1,5 +1,9 @@
 import { start } from '@cbz-tool/server';
 import { app, BrowserWindow } from 'electron';
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.commandLine.appendSwitch('disable-gpu-shader-disk-cache');
 
@@ -9,7 +13,11 @@ let server;
 
 app.whenReady().then(() => {
   server = start(PORT);
-  const win = new BrowserWindow({ width: 1200, height: 800 });
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    icon: join(__dirname, '../ui/public/favicon.ico'),
+  });
 
   server.on('listening', () => {
     // Dev: load Vite for hot reload. Prod: load Express, which serves built files.
